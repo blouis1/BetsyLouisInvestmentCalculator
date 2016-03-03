c1a4798c2e9a9d8aa354532b7f69a1482d9ce2c1
Betsy Louis <blouis1@my.westga.edu>
2016-03-03 12:54:41 -0500

    Added instrumentation test class.

---
+++ app/src/androidTest/java/edu/westga/betsylouisinvestmentcalculator/MainActivityTests.java
@@ -0,0 +1,13 @@
+package edu.westga.betsylouisinvestmentcalculator;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by Betsy on 3/3/2016.
+ */
+public class MainActivityTests extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainActivityTests() {
+        super(MainActivity.class);
+    }
+}
