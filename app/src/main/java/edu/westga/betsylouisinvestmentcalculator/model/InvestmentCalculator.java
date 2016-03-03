f3d01ec1c6aa76f250b51dd57f893117cfae50d6
Betsy Louis <blouis1@my.westga.edu>
2016-03-03 11:43:37 -0500

    Setter for rate sets the correct value for rate.

--- app/src/main/java/edu/westga/betsylouisinvestmentcalculator/model/InvestmentCalculator.java
+++ app/src/main/java/edu/westga/betsylouisinvestmentcalculator/model/InvestmentCalculator.java
@@ -4,4 +4,36 @@ package edu.westga.betsylouisinvestmentcalculator.model;
  * Created by Betsy on 3/3/2016.
  */
 public class InvestmentCalculator {
+    private double principal;
+    private double rate;
+    private int numberOfPeriods;
+
+    public double getPrincipal() {
+        return this.principal;
+    }
+
+    public void setPrincipal(double principal) {
+        this.principal = principal;
+    }
+
+    public double getRate() {
+        return this.rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate/100.0;
+    }
+
+    public int getNumberOfPeriods() {
+        return this.numberOfPeriods;
+    }
+
+    public void setNumberOfPeriods(int numberOfPeriods) {
+        this.numberOfPeriods = numberOfPeriods;
+    }
+
+    public double getFutureValueOfAnnuity() {
+        return this.principal * ((Math.pow((1 + this.rate), this.numberOfPeriods) - 1)/this.rate);
+
+    }
 }
--- app/src/test/java/edu/westga/betsylouisinvestmentcalculator/CalculateFutureValueTests.java
+++ app/src/test/java/edu/westga/betsylouisinvestmentcalculator/CalculateFutureValueTests.java
@@ -2,14 +2,27 @@ package edu.westga.betsylouisinvestmentcalculator;
 
 import org.junit.Test;
 
+import edu.westga.betsylouisinvestmentcalculator.model.InvestmentCalculator;
+
 import static org.junit.Assert.*;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class CalculateFutureValueTests {
+
+    private InvestmentCalculator calculator;
+
+    public CalculateFutureValueTests() {
+        this.calculator = new InvestmentCalculator();
+    }
+
+    /*
+    Test method for BetsyLouisInvestmentCalculator.model.InvestmentCalculator.setRate(java.lang.Double)
+     */
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void testSetterSetsCorrectRate() {
+        this.calculator.setRate(2);
+        assertEquals(0.02, this.calculator.getRate(), 0);
     }
 }
